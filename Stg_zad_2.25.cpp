f6ab54afa036ec4c3669cbd4ea2b56b993658feb
Powerekk <powerbird1234@gmail.com>
2023-03-29 20:12:50 +0200

    Stg zad 2.25 C++

---
+++ Stg_zad_2.25.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+
+int main() {
+    std::cout << "\nStarogard Gdanski: \n" << std::endl;
+    std::cout << "Liczba ludnosci (30.06.2021) - 47,272\n" << std::endl;
+    std::cout << "Wspolrzedne geograficzne: \n- 53o57'47''N\n- 18o31'33''E\n" << std::endl;
+    std::cout << "Powierzchnia - 25,28km2\n" << std::endl;
+
+    system("pause");
+}
---
+++ info.md
@@ -0,0 +1,3 @@
+<h1>Zadanie 2.25 Starogard Gdański</h1>
+
+<h2>Aplikacja w C++ pokazuje informacje na temat miasta Starogard Gdański</h2>
